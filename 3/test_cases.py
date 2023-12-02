6506e12769f0fe970181f26489033061784ad8ae
Uzaif Ahmed <51610781+UzaifAhmed@users.noreply.github.com>
2023-12-02 22:19:20 +0500

    pytest testing functions

--- 3/test_cases.py
+++ 3/test_cases.py
@@ -5,14 +5,14 @@ def test_add():
     assert False
     assert calc.add(4, 5) == 9
 
-def test_add():
+def test_sub():
     assert calc.sub(4, 5) == -1
 
-def test_add():
+def test_mul():
     pass
     assert calc.mul(4, 5) == 19
 
-def test_add():
+def test_div():
     assert calc.divide(10, 5) == 2
 
 
