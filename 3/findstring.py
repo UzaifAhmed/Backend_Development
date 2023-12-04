61d886815e4772feebe83d56ad31048408f6f127
Uzaif Ahmed <51610781+UzaifAhmed@users.noreply.github.com>
2023-12-04 23:28:00 +0500

    Delete 3 directory

--- 3/findstring.py
+++
@@ -1,6 +0,0 @@
-def ispresent(person):
-    names = ['Al', 'Bo', 'Chi', 'Go']
-    return True if person in names else False
-
-def nodigit(person):
-    return True if person.isalpha() else False
--- 3/test_findstring.py
+++
@@ -1,9 +0,0 @@
-from curses.ascii import isdigit
-import findstring
-import pytest
-
-def test_ispresent():
-    assert findstring.ispresent('Al')
-
-def test_nodigit():
-    assert findstring.nodigit('F7')
