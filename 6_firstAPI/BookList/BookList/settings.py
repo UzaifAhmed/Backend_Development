e7303f1efea787cf2a60638f9cb8b3a301ca395d
Uzaif Ahmed <51610781+UzaifAhmed@users.noreply.github.com>
2024-01-18 23:23:52 +0500

    meta

--- 6_firstAPI/BookList/BookList/settings.py
+++ 6_firstAPI/BookList/BookList/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BookListAPI',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- 6_firstAPI/BookList/BookListAPI/views.py
+++ 6_firstAPI/BookList/BookListAPI/views.py
@@ -6,23 +6,34 @@ from .models import Book
 from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
 
+# DJANGO REST FRAMEWORK (DRF) Imports
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
 # Create your views here.
 
-@csrf_exempt
-def books(request):
-    if request.method == 'GET':
-        books = Book.objects.all().values()
-        return JsonResponse({'books': list(books)})
+# @csrf_exempt
+# def books(request):
+#     if request.method == 'GET':
+#         books = Book.objects.all().values()
+#         return JsonResponse({'books': list(books)})
 
-    elif request.method == 'POST':
-        title = request.POST.get('title')
-        author = request.POST.get('author')
-        price = request.POST.get('price')
-        book = Book.object(title=title, author=author, price=price)
+#     elif request.method == 'POST':
+#         title = request.POST.get('title')
+#         author = request.POST.get('author')
+#         price = request.POST.get('price')
+#         book = Book.object(title=title, author=author, price=price)
 
-        try:
-            book.save()
-        except IntegrityError as e:
-            return JsonResponse({'error':'true','message': e},status=400)
+#         try:
+#             book.save()
+#         except IntegrityError as e:
+#             return JsonResponse({'error':'true','message': e},status=400)
     
-    return JsonResponse(model_to_dict(books), status=400)
+    # return JsonResponse(model_to_dict(books), status=400)
+
+@api_view(['POST'])
+def books(request):
+    return Response('List of the books', 
+                    status=status.HTTP_200_OK)
