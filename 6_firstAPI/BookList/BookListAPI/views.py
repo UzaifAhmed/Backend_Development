20645542f575a78acf7d21cba2f8eba2c5ec186c
Uzaif Ahmed <51610781+UzaifAhmed@users.noreply.github.com>
2024-01-20 18:25:22 +0500

    meta

--- 6_firstAPI/BookList/BookListAPI/views.py
+++ 6_firstAPI/BookList/BookListAPI/views.py
@@ -10,6 +10,7 @@ from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -33,7 +34,23 @@ from rest_framework.decorators import api_view
     
     # return JsonResponse(model_to_dict(books), status=400)
 
-@api_view(['POST'])
+@api_view(['GET','POST'])
 def books(request):
     return Response('List of the books', 
                     status=status.HTTP_200_OK)
+
+class BookList(APIView):
+    def get(self, request):
+        author = request.GET.get('author')
+        if(author):
+            return Response({"message":"list of the books by " + author}, status.HTTP_200_OK)
+        
+        return Response({"message":"list of the books"}, status.HTTP_200_OK)
+    
+    def post(self, request):
+        return Response({"message":request.data.get('title')}, status.HTTP_201_OK)
+    
+
+class Book(APIView):
+    def get(self, request, pk):
+        return Response({"message":"single book with id " + str(pk)}, status.HTTP_200_OK)
